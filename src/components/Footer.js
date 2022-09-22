f87083be8aefce3fe209fe67c34f4c627c136614
rjawa07 <jawarudy07@gmail.com>
2022-09-22 16:47:12 +0530

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import React from 'react'
 function Footer() {
     return (<>
         <footer>
-            <h3>Made by Rohit Jawa</h3>
+            <h3>Made by Prakash Dhayal</h3>
             <h4>Software Engineering Project</h4>
-            <p>Contact Me: <a href="https://www.linkedin.com/in/rohit-jawa-73b043193/" target="_blank">LinkedIn</a></p>
+            <p>Contact Me: <a href="https://www.linkedin.com/prakash-dhayal/" target="_blank">LinkedIn</a></p>
         </footer>
     </>);
 }
